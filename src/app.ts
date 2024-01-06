df2e7969945df6884181f734434f0c7624071e23
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-06 23:36:13 +0530

    connect mongodb

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/codecollab",);
+
+export default app;
